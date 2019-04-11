18bccc7f60acc7fe8a764d37f117c1219bd6b62b
SIeep <brandon_n_evans@live.com>
2019-04-10 19:10:04 -0500

    add loop to twilio controller to text all contacts of logged in user

--- avert_relapse/app/controllers/api/twilio_controller.rb
+++ avert_relapse/app/controllers/api/twilio_controller.rb
@@ -9,12 +9,18 @@ class Api::TwilioController < ApplicationController
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
     from = '+17372042024' # My Twilio number
-    to = '+1' + current_user.account.contacts[1].phone_number # My mobile phone number, sends to my phone number for test
 
-    client.messages.create(
-    from: from,
-    to: to,
-    body: "Hello! This is " + current_user.account.name + ". I'm using an app called Avert Relapse and have pressed the 'HELP' button which means I need help! Please text me back at my phone number which is:" + current_user.account.phone_number + " // THIS IS A TEST //"
-    )
+    if current_user
+      from = '+17372042024'
+      i = 0
+      current_user.account.contacts.length.times do
+        to = '+1' + current_user.account.contacts[i].phone_number
+        i += 1
+        client.messages.create(
+        from: from,
+        to: to,
+        body: "Hello! This is " + current_user.account.name + ". I'm using an app called Avert Relapse and have pressed the 'HELP' button which means I need help! Please text me back at my phone number which is:" + current_user.account.phone_number + " // THIS IS A TEST //")
+      end
+    end
   end
 end
