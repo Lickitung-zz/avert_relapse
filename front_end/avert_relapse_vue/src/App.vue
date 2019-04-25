27e9f30ca663d58efa9298c34780fa03600a8367
Brandon Evans <bnevans@fb.com>
2019-04-25 13:35:05 -0500

    add placeholder logo for nav

--- front_end/avert_relapse_vue/src/App.vue
+++ front_end/avert_relapse_vue/src/App.vue
@@ -18,7 +18,8 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index-register.html"><img src="HTML/images/logo.png" alt="logo" /></a>
+            <img src="HTML/images/logo.png" alt="logo" /></a>
+            <a class="navbar-brand" href="index-register.html"></a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
