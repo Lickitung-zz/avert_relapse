3d28a487cc2cd09b251b7c731bad62c1dee731cd
SIeep <brandon_n_evans@live.com>
2019-04-26 18:34:56 -0500

    make profile name dynamic on TimelineAbout.vue

--- front_end/avert_relapse_vue/src/views/TimelineAbout.vue
+++ front_end/avert_relapse_vue/src/views/TimelineAbout.vue
@@ -16,7 +16,9 @@
               <div class="col-md-3">
                 <div class="profile-info">
                   <img src="http://placehold.it/300x300" alt="" class="img-responsive profile-photo" />
-                  <h3>Sarah Cruiz</h3>
+                  <div v-for="account in accounts">
+                    <h3>{{ accounts.name }}</h3>
+                  </div>
                   <p class="text-muted">Creative Director</p>
                 </div>
               </div>
@@ -151,3 +153,106 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Avert.relapse!",
+      contacts: [],
+      messages: "",
+      accounts: [],
+      loginEmail: "",
+      loginPassword: "",
+      help: "",
+      newContactFirstName: "",
+      newContactLastName: "",
+      newContactPhoneNumber: "",
+      newContactEmail: "",
+      newMessage: "",
+      // newContactAccountId: User.account.id,
+      errors: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {this.contacts = response.data;
+    });
+    axios.get("/api/twilio/sms").then(response => {
+      this.messages = response.data;
+    });
+    axios.get("/api/accounts/show").then(response => {
+      this.accounts = response.data;
+    });
+    // axios.get("http://localhost:3000/api/twilio/sms").then(response => {this.texts = response.data;
+    // });
+  },
+  methods: {
+    login: function() {
+      var params = {
+        email: this.loginEmail,
+        password: this.loginPassword
+      };
+      console.log('logging in...');
+      axios.post('/api/sessions', params).then(response => {
+        console.log(response);
+      });
+    },
+    sendHelp: function() {
+      console.log("sending help to all friends...");
+      axios.post("http://localhost:3000/api/twilio/sms").then(response => {
+        console.log(response);
+        console.log("sent text to all friends");
+      });
+    },
+    createContact: function() {
+      var params = {
+        first_name: this.newContactFirstName,
+        last_name: this.newContactLastName,
+        phone_number: this.newContactPhoneNumber,
+        email: this.newContactEmail
+      };
+      console.log('adding contact...');
+      axios.post("/api/contacts", params).then(
+        response => {
+          console.log(response);
+          this.$router.push("/index");
+        }).catch(error => {
+        console.log("this isn't working.");
+        console.log(error.response.data.errors);
+        this.error = error.response.data.errors;
+      });
+    },
+    deleteContact: function(contact) {
+      console.log("deleting contact...");
+      axios.delete("/api/contacts/" + contact.id).then(response => {
+        var index = this.contacts.indexOf(
+          contact);
+        this.contacts.splice(index, 1);
+      });
+    },
+    editContact: function(contact) {
+      var params = {
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        phone_number: contact.phone_number,
+        email: contact.email
+      };
+      axios.patch("/api/contacts/" + contact.id, params).then(response => {
+        console.log(response);
+      });
+    },
+    updateMessage: function() {
+      var params = {
+        messages: this.messages.help_message
+      };
+      console.log('updating the message...');
+
+      axios.post("/api/twilio/sms_update", params).then(response => {
+          console.log(response);
+      });
+    }
+  }
+};
+</script>
