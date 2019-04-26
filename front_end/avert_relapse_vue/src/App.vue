e601978ca3a16e20230325c1f1e1376f3b6ae4e9
Brandon Evans <bnevans@fb.com>
2019-04-26 12:51:35 -0500

    fix styling for logout/login on navbar

--- front_end/avert_relapse_vue/src/App.vue
+++ front_end/avert_relapse_vue/src/App.vue
@@ -57,30 +57,11 @@
                   <li><a href="edit-profile-password.html">Change Password</a></li>
                 </ul>
               </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle pages" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">All Pages <span><img src="HTML/images/down-arrow.png" alt="" /></span></a>
-                <ul class="dropdown-menu page-list">
-                  <li><a href="index.html">Landing Page 1</a></li>
-                  <li><a href="index-register.html">Landing Page 2</a></li>
-                  <li><a href="newsfeed.html">Newsfeed</a></li>
-                  <li><a href="newsfeed-people-nearby.html">Poeple Nearly</a></li>
-                  <li><a href="newsfeed-friends.html">My friends</a></li>
-                  <li><a href="newsfeed-messages.html">Chatroom</a></li>
-                  <li><a href="newsfeed-images.html">Images</a></li>
-                  <li><a href="newsfeed-videos.html">Videos</a></li>
-                  <li><a href="timeline.html">Timeline</a></li>
-                  <li><a href="timeline-about.html">Timeline About</a></li>
-                  <li><a href="timeline-album.html">Timeline Album</a></li>
-                  <li><a href="timeline-friends.html">Timeline Friends</a></li>
-                  <li><a href="edit-profile-basic.html">Edit Profile</a></li>
-                  <li><a href="contact.html">Contact Us</a></li>
-                  <li><a href="faq.html">FAQ Page</a></li>
-                  <li><a href="404.html">404 Not Found</a></li>
-                </ul>
-              </li>
-              <li class="dropdown"><div v-if="is_logged_in === true">
+              <!-- if logged in navbar begin -->
+              <li class="dropdown"><a v-if="is_logged_in === true">
                 <router-link to="/logout">Logout</router-link>
-              </div></li>
+              </a></li>
+              <!-- if logged in navbar end -->
             </ul>
             <form class="navbar-form navbar-right hidden-sm">
               <div class="form-group">
