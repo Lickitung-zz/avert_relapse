af7585728cdcef954e856c0d5d84d85e7ad39d68
Brandon Evans <bnevans@fb.com>
2019-04-26 15:18:03 -0500

    add form to addfriend.vue

--- front_end/avert_relapse_vue/src/views/AddFriend.vue
+++ front_end/avert_relapse_vue/src/views/AddFriend.vue
@@ -2,21 +2,133 @@
 	<div class="newFriend">
 	    <h1>This is the add friends page</h1>
 	    <h1>This is the add friends page</h1>
-	    <h1>This is the add friends page</h1>
+	    <div class="form-group">
+		<h4>Add friend</h4>
+		  <form>
+		    <p>First Name <input type=text v-model="newContactFirstName"></p>
+		    <p>Last Name <input type=text v-model="newContactLastName"></p>
+		    <p>Phone Number <input type=text v-model="newContactPhoneNumber"></p>
+		    <p>Email <input type=text v-model="newContactEmail"></p>
+		    <button v-on:click="createContact()">Add Friend</button>
+		  </form>
+		</div>
 	</div>
 </template>
 
 
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Avert.relapse!",
+      contacts: [],
+      messages: "",
+      accounts: [],
+      loginEmail: "",
+      loginPassword: "",
+      help: "",
+      newContactFirstName: "",
+      newContactLastName: "",
+      newContactPhoneNumber: "",
+      newContactEmail: "",
+      newMessage: "",
+      // newContactAccountId: User.account.id,
+      errors: []
+    };
+  },
+  created: function() {
+    axios.get("/api/contacts").then(response => {
+      this.contacts = response.data;
+    });
+    axios.get("/api/twilio/sms").then(response => {
+      this.messages = response.data;
+    });
+    axios.get("/api/accounts/show").then(response => {
+      this.accounts = response.data;
+    });
+    // axios.get("http://localhost:3000/api/twilio/sms").then(response => {this.texts = response.data;
+    // });
+  },
+  methods: {
+    login: function() {
+      var params = {
+        email: this.loginEmail,
+        password: this.loginPassword
+      };
+      console.log('logging in...');
+      axios.post('/api/sessions', params).then(response => {
+        console.log(response);
+      });
+    },
+    sendHelp: function() {
+      console.log("sending help to all contacts...");
+      axios.post("http://localhost:3000/api/twilio/sms").then(response => {
+        console.log(response);
+        console.log("sent text to all contacts");
+      });
+    },
+    createContact: function() {
+      var params = {
+        first_name: this.newContactFirstName,
+        last_name: this.newContactLastName,
+        phone_number: this.newContactPhoneNumber,
+        email: this.newContactEmail
+      };
+      console.log('adding contact...');
+      axios.post("/api/contacts", params).then(
+        response => {
+          console.log(response);
+          this.$router.push("/index");
+        }).catch(error => {
+        console.log("this isn't working.");
+        console.log(error.response.data.errors);
+        this.error = error.response.data.errors;
+      });
+    },
+    deleteContact: function(contact) {
+      console.log("deleting contact...");
+      axios.delete("/api/contacts/" + contact.id).then(response => {
+        var index = this.contacts.indexOf(
+          contact);
+        this.contacts.splice(index, 1);
+      });
+    },
+    getContact: function() {
+      var params = {
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        phone_number: contact.phone_number,
+        email: contact.email
+      };
+      axios.get("api/contacts/" + contact.id, params).then(response => {
+        console.log(response)
+      });
+    },
+    editContact: function(contact) {
+      var params = {
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        phone_number: contact.phone_number,
+        email: contact.email
+      };
+      axios.patch("/api/contacts/" + contact.id, params).then(response => {
+        console.log(response);
+      });
+    },
+    updateMessage: function() {
+      var params = {
+        messages: this.messages.help_message
+      };
+      console.log('updating the message...');
+
+      axios.post("/api/twilio/sms_update", params).then(response => {
+          console.log(response);
+      });
+    }
+  }
+};
+</script>
 
 
-
-<div class="form-group">
-<h4>Add friend</h4>
-  <form>
-    <p>First Name <input type=text v-model="newContactFirstName"></p>
-    <p>Last Name <input type=text v-model="newContactLastName"></p>
-    <p>Phone Number <input type=text v-model="newContactPhoneNumber"></p>
-    <p>Email <input type=text v-model="newContactEmail"></p>
-    <button v-on:click="createContact()">Add Friend</button>
-  </form>
-</div>
