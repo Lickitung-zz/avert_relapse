da5a5464f7bc7e7488fa713be64897458493702f
Brandon Evans <bnevans@fb.com>
2019-04-30 10:26:13 -0500

    remove algolia api

--- avert_relapse/app/models/account.rb
+++ avert_relapse/app/models/account.rb
@@ -2,10 +2,4 @@ class Account < ApplicationRecord
   belongs_to :user
   has_many :contacts
   has_many :friends
-
-  include AlgoliaSearch
-
-  algoliasearch do
-    attributes :first_name, :last_name
-  end
 end
