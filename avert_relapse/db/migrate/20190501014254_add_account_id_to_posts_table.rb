7cecf6fe9d05b11cb9c0c551aebeee3c8f81c0db
SIeep <brandon_n_evans@live.com>
2019-04-30 20:44:55 -0500

    change :account_id to integer rather than string for Posts table

---
+++ avert_relapse/db/migrate/20190501014254_add_account_id_to_posts_table.rb
@@ -0,0 +1,5 @@
+class AddAccountIdToPostsTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :posts, :account_id, :integer
+  end
+end
--- avert_relapse/db/schema.rb
+++ avert_relapse/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_012955) do
+ActiveRecord::Schema.define(version: 2019_05_01_014254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 2019_05_01_012955) do
   create_table "posts", force: :cascade do |t|
     t.string "text"
     t.string "published_date"
-    t.string "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "account_id"
   end
 
   create_table "users", force: :cascade do |t|
