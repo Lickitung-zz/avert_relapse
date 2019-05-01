c1e3a1b1a4f4b39b1704c971fde85cca5f17f4ba
SIeep <brandon_n_evans@live.com>
2019-04-30 21:14:53 -0500

    add Images model and api/images controller

--- avert_relapse/db/schema.rb
+++ avert_relapse/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_014254) do
+ActiveRecord::Schema.define(version: 2019_05_01_021347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2019_05_01_014254) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "image_source"
+    t.integer "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
