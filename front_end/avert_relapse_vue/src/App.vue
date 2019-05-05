3860b2980f17e761a1afd931364ec3928cc78356
SIeep <brandon_n_evans@live.com>
2019-05-04 22:42:33 -0500

    re-do entire navbar

--- front_end/avert_relapse_vue/src/App.vue
+++ front_end/avert_relapse_vue/src/App.vue
@@ -1,102 +1,79 @@
 <template>
   <div id="app">
 
-    <!-- Header
-    ================================================= -->
-    <header id="header">
-      <nav class="navbar navbar-default navbar-fixed-top menu">
-        <div class="container">
-
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
+     <!-- commented out color picker for nav bar below  -->
+     <!-- <input class="input-color" v-model="colorx" type="color" name="" value=""> -->
+    <vs-navbar
+      v-model="indexActive"
+      :color="colorx"
+      text-color="rgba(255,255,255,.6)"
+      active-text-color="rgba(255,255,255,1)"
+      class="myNavbar">
+      <div slot="title">
+        <vs-navbar-title>
+          <!-- <div class="navbar-header"> -->
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <img src="HTML/images/logo.png" alt="logo" />
-            <a class="navbar-brand" href="/index"></a>
-          </div>
+            <!-- <img src="HTML/images/logo.png" alt="logo" /> -->
+            <!-- <a class="navbar-brand" href="/index"></a> -->
+          <!-- </div> -->
+        </vs-navbar-title>
+      </div>
 
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right main-menu">
-              <!-- <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Newsfeed <span><img src="HTML/images/down-arrow.png" alt="" /></span></a>
-                <ul class="dropdown-menu newsfeed-home">
-                  <li><a href="newsfeed.html">Newsfeed</a></li>
-                  <li><a href="newsfeed-people-nearby.html">Poeple Nearly</a></li>
-                  <li><a href="newsfeed-friends.html">My friends</a></li>
-                  <li><a href="newsfeed-messages.html">Chatroom</a></li>
-                  <li><a href="newsfeed-images.html">Images</a></li>
-                  <li><a href="newsfeed-videos.html">Videos</a></li>
-                </ul>
-              </li> -->
-              <!-- li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Timeline <span><img src="HTML/images/down-arrow.png" alt="" /></span></a>
-                <ul class="dropdown-menu login">
-                  <li><a href="timeline.html">Timeline</a></li>
-                  <li><a href="timeline-about.html">Timeline About</a></li>
-                  <li><a href="timeline-album.html">Timeline Album</a></li>
-                  <li><a href="timeline-friends.html">Timeline Friends</a></li>
-                  <li><a href="edit-profile-basic.html">Edit: Basic Info</a></li>
-                  <li><a href="edit-profile-work-edu.html">Edit: Work</a></li>
-                  <li><a href="edit-profile-interests.html">Edit: Interests</a></li>
-                  <li><a href="edit-profile-settings.html">Account Settings</a></li>
-                  <li><a href="edit-profile-password.html">Change Password</a></li>
-                </ul>
-              </li> -->
-              <!-- if logged in navbar begin -->
-              <li class="dropdown"><a v-if="is_logged_in === true">
-                <router-link to="/index">Home</router-link>
-              </a></li>
-              <li class="dropdown"><a v-if="is_logged_in === true">
-                <router-link to="/contacts">Contacts</router-link>
-              </a></li>
-              <li class="dropdown"><a v-if="is_logged_in === true">
-                <router-link to="/help">Help</router-link>
-              </a></li>
-              <li class="dropdown"><a v-if="is_logged_in === true">
-                <router-link to="/messages-index">Messages</router-link>
-              </a></li>
-              <li class="dropdown"><a v-if="is_logged_in === true">
-                <router-link to="/settings">Settings</router-link>
-              </a></li>
-              <li class="dropdown">
-                <a href="/index" role="button" aria-haspopup="true" aria-expanded="false"> <span></span></a>
-              </li>
-              <li class="dropdown">
-                <a v-if="is_logged_in === true">
-                  <router-link to="/logout">Logout</router-link>
-                </a>
-                <a v-else>
-                  <router-link to="/login">Login</router-link>
-                </a>
-              </li>
-              <!-- if logged in navbar end -->
-
-            </ul>
-            <form class="navbar-form navbar-right hidden-sm" v-on:enter.prevent="searching()">
-
-              <!-- div class="form-group">
-                <i class="icon ion-android-search"></i>
-                <input id="my-search" class="form-control input-group-lg" type="search" name="Searchsss" title="Search here" placeholder="Search for friends and accounts" v-model="searchQuery"/>
-              </div> -->
-            </form>
-
-            <form class="navbar-form" v-on:submit.prevent="searching()" name="search_form" id='search_form'>
-             <div class="row">
-              <div class="form-group">
-                <i class="icon ion-android-search"></i>
-                <input id="my-search" class="form-control input-group-lg" type="search" name="Searchsss" title="Search here" placeholder="Search for friends and accounts" v-model="searchQuery"/>
+      <vs-navbar-item index="0" >
+        <a v-if="is_logged_in === true">
+          <router-link to="/index" style="font-size: 1.1rem">Home</router-link>
+        </a>
+      </vs-navbar-item>
+      <vs-navbar-item index="1">
+        <a v-if="is_logged_in === true">
+          <router-link to="/contacts" style="font-size: 1.1rem">Contacts</router-link>
+        </a>
+      </vs-navbar-item>
+      <vs-navbar-item index="2">
+        <a v-if="is_logged_in === true">
+          <router-link to="/help" style="font-size: 1.1rem">Help</router-link>
+        </a>
+      </vs-navbar-item>
+      <vs-navbar-item index="3">
+        <a v-if="is_logged_in === true">
+          <router-link to="/messages-index" style="font-size: 1.1rem">Messages</router-link>
+        </a>
+      </vs-navbar-item>
+      <vs-navbar-item index="4">
+        <!-- <a href="#">Update</a> -->
+        <a class="dropdown" v-if="is_logged_in === true">
+          <router-link to="/settings" style="font-size: 1.1rem">Settings</router-link>
+        </a>
+      </vs-navbar-item>
+      <vs-navbar-item index="5">
+        <a v-if="is_logged_in === true">
+          <router-link to="/logout" style="font-size: 1.1rem">Logout</router-link>
+        </a>
+        <a v-else>
+          <router-link to="/login" style="font-size: 1.1rem">Login</router-link>
+        </a>
+      </vs-navbar-item>
+      
+      <form class="navbar-form" v-on:submit.prevent="searching()" name="search_form" id='search_form'>
+             
+              
+                <vs-input id="my-search" type="search" icon="search" style="font-size: 1.1rem" placeholder="Search for friends!" v-model="searchQuery"/>
+                <!-- <input id="my-search" class="form-control input-group-lg" type="search" name="Searchsss" title="Search here" placeholder="Search for friends and accounts" v-model="searchQuery"/> -->
                 <!-- <input type="submit" name="searching"> -->
-              </div>
-            </div>
+              
+            
           </form><!--Login Form Ends--> 
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container -->
-      </nav>
+      <vs-spacer></vs-spacer>
+
+      <vs-button color-text="rgb(255, 255, 255)" color="rgba(255, 255, 255, 0.3)" type="flat" icon="more_horiz"></vs-button>
+    </vs-navbar>
+
+    
     </header>
     <!--Header End-->
 
@@ -196,6 +173,8 @@ export default {
       avatar: null,
       saving: false,
       saved: false,
+      colorx: '#212322',
+      indexActive: 0,
       // newContactAccountId: User.account.id,
       errors: []
     };
@@ -299,3 +278,4 @@ export default {
   }
 };
 </script>
+
